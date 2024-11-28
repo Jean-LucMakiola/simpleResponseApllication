13d3999cdd9594451803c570ef2bf9347bced805
Jean-Luc Makiola <jeanlucmakiola@proton.me>
2024-11-28 07:30:24 +0100

    Added Module Device, with HTTP mappings

--- src/main/java/com/example/simpleresponse/springapi/api/repo/DeviceRepo.java
+++ src/main/java/com/example/simpleresponse/springapi/api/repo/DeviceRepo.java
@@ -1,7 +1,6 @@
 package com.example.simpleresponse.springapi.api.repo;
 
 import com.example.simpleresponse.springapi.api.model.Device;
-import com.example.simpleresponse.springapi.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
